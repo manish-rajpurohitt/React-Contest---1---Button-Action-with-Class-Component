64d32cb5122f6e0d18c6fe90b60cfe3072c4f15f
Manish Purohit <manishkumar9866@gmail.com>
2020-10-24 19:15:58 +0530

    NS Final Submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,34 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    constructor(props) {
-		super(props);
-		this.state = {
-			rend: <></>
-		  };
-	handleFun = () =>{
-		this.setState({rend:<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>});
-	}  
-	};
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rend: <></>
+    };
 
-	
-	
-    render() {
-    	return(
-    		<div id="main">
-				<button onClick="handleFun" id="click">click</button>
-				{ /* Do not remove this main div!! */ }
-				{this.state.rend}
-    		</div>
-    	);
-    }
+    this.handleFun = () => {
+      this.setState({
+        rend: (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        )
+      });
+    };
+  }
+
+  render() {
+    return (
+      <div id="main">
+        <button onClick={this.handleFun.bind(this)} id="click">
+          click
+        </button>
+        {/* Do not remove this main div!! */}
+        {this.state.rend}
+      </div>
+    );
+  }
 }
