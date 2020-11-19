dbef2b4589582307abbdd1afb4f1657913adbd19
Manish Purohit <manishkumar9866@gmail.com>
2020-11-19 13:33:10 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -5,29 +5,33 @@ export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rend: ""
+      rend: false
     };
-
     this.handleFun = () => {
       this.setState({
-        rend: (
-          <p id="para">
-            Hello, I've learnt to use the full-stack evaluation tool. This makes
-            me so happy
-          </p>
-        )
+        rend: true
       });
     };
+    this.handleFun = this.handleFun.bind(this);
   }
 
   render() {
     return (
       <div id="main">
-        <button onClick={this.handleFun.bind(this)} id="click">
+        <button onClick={() => this.handleFun()} id="click">
           click
         </button>
         {/* Do not remove this main div!! */}
-        {this.state.rend}
+        {this.state.rend ? (
+          <>
+            <p id="para">
+              Hello, I've learnt to use the full-stack evaluation tool. This
+              makes me so happy
+            </p>
+          </>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
